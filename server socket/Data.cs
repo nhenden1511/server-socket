c5f283584bc6941a90156170f55709bfc28f0b0f
HaoNguyen <nhenden1511@gmail.com>
2017-12-27 18:51:29 +0700

    create

---
+++ server socket/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace server_socket
+{
+    public static class DataTest
+    {
+        
+
+        public static string DATA = "Prime Minister Nguyễn Xuân Phúc (R) receives Japanese Minister of Economic Revitalisation, Toshimitsu Motegi, in Hà Nội yesterday.— VNA/VNS Photo Thống Nhất HÀ NỘI — Việt Nam and Japan should take the lead in speeding up the signing of the Comprehensive and Progressive Agreement for Trans-Pacific Partnership(CPTPP) as the two nations have a strategic partnership, traditional friendship and time-honoured trade, economic and investment ties, said Prime Minister Nguyễn Xuân Phúc. While receiving Japanese Minister of Economic Revitalisation, Toshimitsu Motegi, in Hà Nội yesterday, Phúc spoke highly of the minister’s visit, saying that it contributed actively to boosting trade exchanges between Việt Nam and Japan as well as among Asia-Pacific economies. Having expressed his delight at the comprehensive and practical development of the Việt Nam-Japan strategic partnership, Phúc hailed the positive outcomes of an earlier working session between Minister Motegi and Vietnamese Minister of Industry and Trade Trần Tuấn Anh.The two ministers discussed measures to effectively address issues hindering bilateral co-operation, and reached consensus on key issues related to the CPTPP, thus promoting the trade exchanges among stakeholders, particularly bilateral co-operative ties between Việt Nam and Japan.For his part, Motegi congratulated the Government and people of Việt Nam on the successful hosting of the APEC 2017 Economic Leaders’ Week, saying that the event enhanced the role and position of Việt Nam in the international arena.He emphasised the role of PM Phúc and Minister Trần Tuấn Anh as well as related agencies of Việt Nam in the negotiation process in meetings within the framework of the APEC 2017 Economic Leaders’ Week, thus helping to form the CPTPP.The minister expressed his hope that Việt Nam-Japan co-operative ties would continue reaping more fruit in the future. mpowered by Japanese Prime Minister Shinzo Abe, the Japanese minister is visiting Việt Nam to discuss bilateral co-operation issues as well as matters related to the CPTPP so that the deal could be signed soon, opening up new co-operation opportunities for its members. — VNSRead more at http://vietnamnews.vn/politics-laws/420169/pm-viet-nam-japan-should-pioneer-in-accelerating-cptpp.html#drtBXJKt4U0YUJfl.99";
+    }
+}
---
+++ server socket/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Threading;
+
+namespace server_socket
+{
+    [Activity(Label = "server_socket", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var _btn1 = FindViewById<Button>(Resource.Id.btn1);
+            var _btn2 = FindViewById<Button>(Resource.Id.btn2);
+            var _btn3 = FindViewById<Button>(Resource.Id.btn3);
+            var _btn4 = FindViewById<Button>(Resource.Id.btn4);
+            var _txt2 = FindViewById<TextView>(Resource.Id.txt2);
+
+
+            _btn1.Click += delegate
+            {
+                AsynchronousSocketListener.StartListening();
+
+            };
+
+            var signal = new ManualResetEvent(false);
+            // or signal = new AutoResetEvent(false);
+
+            
+            _btn2.Click += delegate
+            {
+                signal.Reset();
+                var newThreead = new Thread(() =>
+                {
+                    RunOnUiThread(() =>
+                    {
+                        _txt2.Text = "Wait....";
+                    });
+
+                    signal.WaitOne();
+
+                    RunOnUiThread(() =>
+                    {
+                        _txt2.Text = "Done...";
+                    });
+                });
+                newThreead.Start();
+                
+            };
+
+            _btn3.Click += delegate
+            {
+                signal.Set();
+            };
+
+
+        }
+    }
+}
+
